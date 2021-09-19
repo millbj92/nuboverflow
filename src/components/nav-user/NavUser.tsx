a6941150505b3bd0c9439b49547c5fba2ae2bf1f
Brandon Miller <millbj92@gmail.com>
2021-09-19 05:39:30 -0400

    refactor styled components

--- src/components/nav-user/NavUser.tsx
+++ src/components/nav-user/NavUser.tsx
@@ -1,11 +1,40 @@
+import { faMedal } from '@fortawesome/pro-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import Gravatar from 'react-gravatar';
+import { useTheme } from 'styled-components';
+import IUser from '../../models/IUser';
 import { GravatarContainer } from '../containers/Gravatar.style';
+import { UserAwardContainer } from '../containers/UserAwardContainer.style';
 
-const NavUser: React.FC = () => {
+interface IUserNavProps {
+    user: IUser;
+}
+
+const NavUser: React.FunctionComponent<IUserNavProps> = (props) => {
+    const { email, userScore, userAwards } = props.user;
+    const { colors } = useTheme();
     return (
-        <div style={{ marginRight: '.5rem' }}>
-            <GravatarContainer email="test@gmail.com" />
-        </div>
+        <>
+            <GravatarContainer>
+                <Gravatar size={40} email={email} />
+            </GravatarContainer>
+            <span style={{ marginRight: '0.5rem' }}>
+                <p style={{ letterSpacing: '1px' }}>{userScore}</p>
+            </span>
+            <UserAwardContainer color={colors.colorGold}>
+                <FontAwesomeIcon icon={faMedal} style={{ marginRight: '5px', marginTop: '3px' }} />
+                {userAwards.filter((x) => x.awardLevel === 2).length}
+            </UserAwardContainer>
+            <UserAwardContainer color={colors.colorSilver}>
+                <FontAwesomeIcon icon={faMedal} style={{ marginRight: '5px', marginTop: '3px' }} />
+                {userAwards.filter((x) => x.awardLevel === 1).length}
+            </UserAwardContainer>
+            <UserAwardContainer color={colors.colorBronze}>
+                <FontAwesomeIcon icon={faMedal} style={{ marginRight: '5px', marginTop: '3px' }} />
+                {userAwards.filter((x) => x.awardLevel === 0).length}
+            </UserAwardContainer>
+        </>
     );
 };
 
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -10,9 +10,56 @@ import { NavButton } from '../buttons/NavButton.style';
 import { faTrophyAlt } from '@fortawesome/pro-regular-svg-icons';
 import { NavUserContainer } from '../containers/NavUserContainer.style';
 import NavUser from '../nav-user/NavUser';
-import { faCertificate, faMedal } from '@fortawesome/pro-solid-svg-icons';
+import IUser from '../../models/IUser';
+//import Badge from '../badge/Badge';
+import Badge from '../badge/Badge.style';
+import { useTheme } from 'styled-components';
+
+const mockUser: IUser = {
+    userName: 'brandon',
+    email: 'test@testemail.com',
+    linkedin: 'https://linkedin.com/millbj92',
+    github: 'https://github.com/millbj92',
+    userScore: 1800,
+    bio: `Hi! I'm Brandon. I've been programming now for about 15 years, and love it! Currently I've been focusing a lot on cloud technology: specifically architecture and development. I know mostly AWS but have worked a lot with Azure as well. Feel free to message me if you need any advice!`,
+    profession: `Princiapal Consultant`,
+    workPlace: 'Technossus',
+    interests: [
+        'C#',
+        'Go',
+        'Web Development',
+        'Javascript',
+        'Typescript',
+        'React',
+        'Angular',
+        'AWS',
+        'Cloud Development',
+        'Cloud Architecture',
+    ],
+    userAwards: [
+        {
+            awardName: 'Getting Started',
+            awardDescription: 'Answer one question correctly',
+            awardLevel: 0,
+            awardScore: 100,
+        },
+        {
+            awardName: 'Test silver Award',
+            awardDescription: 'Silver award desc',
+            awardLevel: 1,
+            awardScore: 300,
+        },
+        {
+            awardName: 'Test Gold award',
+            awardDescription: 'gold award desc',
+            awardLevel: 2,
+            awardScore: 500,
+        },
+    ],
+};
 
 const Navbar: React.FC = () => {
+    const { colors } = useTheme();
     return (
         <NavContainer>
             <div style={{ display: 'flex', width: '100%' }}>
@@ -22,38 +69,11 @@ const Navbar: React.FC = () => {
                 <Searchbar></Searchbar>
             </div>
             <NavUserContainer>
-                <NavUser></NavUser>
-                <span style={{ marginRight: '0.5rem' }}>
-                    <strong>1,800</strong>
-                </span>
-                <span style={{ color: 'gold', display: 'inline-flex', marginRight: '.5rem', fontSize: '1.2rem' }}>
-                    <FontAwesomeIcon icon={faMedal} /> 1
-                </span>
-                <span style={{ color: '#B4B8BC', display: 'inline-flex', marginRight: '.5rem', fontSize: '1.2rem' }}>
-                    <FontAwesomeIcon icon={faMedal} /> 8
-                </span>
-                <span style={{ color: '#CD7F32', display: 'inline-flex', marginRight: '.5rem', fontSize: '1.2rem' }}>
-                    <FontAwesomeIcon icon={faMedal} /> 32
-                </span>
+                <NavUser user={mockUser}></NavUser>
             </NavUserContainer>
             <NavButtonContainer>
-                <NavButton style={{ position: 'relative' }}>
-                    <div style={{ position: 'absolute', top: '20%', right: '2px', zIndex: 10 }}>
-                        <div style={{ position: 'relative' }}>
-                            <span
-                                style={{
-                                    position: 'absolute',
-                                    top: '1px',
-                                    right: '0.5px',
-                                    zIndex: 11,
-                                    fontSize: '14px',
-                                }}
-                            >
-                                1
-                            </span>
-                            <FontAwesomeIcon style={{ fontSize: '18px', color: 'gold' }} icon={faCertificate} />
-                        </div>
-                    </div>
+                <NavButton>
+                    <Badge notificationCount={1} badgeColor={colors.colorGold} />
                     <FontAwesomeIcon icon={faComment} />
                 </NavButton>
                 <NavButton>
